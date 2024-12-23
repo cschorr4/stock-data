00936e1c8ebce8f6aeec86ad027b48a07a09a689
cschorr4 <cschorr@purdue.edu>
2024-12-23 15:39:05 -0500

    scroll

--- types/swiper.d.ts
+++ types/swiper.d.ts
@@ -1,14 +1,26 @@
-interface SwiperContainerElement extends HTMLElement {
-    initialize: () => void;
+import React from 'react';
+
+interface SwiperParams {
+  init: string;
+  class: string;
+  slidesPerView?: number;
+  spaceBetween?: number;
+  loop?: boolean;
+  autoplay?: {
+    delay: number;
+    disableOnInteraction: boolean;
+  };
+  pagination?: {
+    clickable: boolean;
+  };
+  speed?: number;
+  effect?: string;
+  grabCursor?: boolean;
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'swiper-container': HTMLElement & SwiperParams & { initialize: () => void };
+    'swiper-slide': HTMLElement;
   }
-  
-  interface SwiperSlideElement extends HTMLElement {}
-  
-  declare global {
-    namespace React {
-      interface HTMLElementTagNameMap {
-        'swiper-container': SwiperContainerElement;
-        'swiper-slide': SwiperSlideElement;
-      }
-    }
-  }
+}
