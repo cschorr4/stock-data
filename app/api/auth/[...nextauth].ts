7373843bf284ef18e9c05668e43877d3ca6cbeb9
cschorr4 <cschorr@purdue.edu>
2024-12-31 09:49:14 -0500

    cards

--- app/api/auth/[...nextauth].ts
+++ app/api/auth/[...nextauth].ts
@@ -1,19 +1,16 @@
-// app/api/auth/[...nextauth]/route.ts
-import NextAuth from 'next-auth'
-import EmailProvider from 'next-auth/providers/email'
-import { PrismaAdapter } from '@next-auth/prisma-adapter'
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
+import NextAuth from "next-auth"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import EmailProvider from "next-auth/providers/email"
+import { prisma } from "@/lib/prisma"
 
 const handler = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
     EmailProvider({
       server: process.env.EMAIL_SERVER,
-      from: process.env.EMAIL_FROM
-    })
-  ]
+      from: process.env.EMAIL_FROM,
+    }),
+  ],
 })
 
 export { handler as GET, handler as POST }
