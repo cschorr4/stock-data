3a1a4fe9b56abed687793bb5a313872a3d5852e8
cschorr4 <cschorr@purdue.edu>
2024-12-31 15:00:45 -0500

    login

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -1,40 +1,39 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
 import { cookies } from "next/headers";
-import { ResponseCookie } from "next/dist/server/web/spec-extension/cookies";
 
 export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
-    return createServerClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-      {
-        cookies: {
-          get(name: string) {
-            return cookieStore.get(name)?.value;
-          },
-          set(name: string, value: string, options: CookieOptions) {
-            try {
-              cookieStore.set({
-                name,
-                value,
-                ...options,
-              });
-            } catch (error) {
-              console.error('Error setting cookie:', error);
-            }
-          },
-          remove(name: string, options: CookieOptions) {
-            try {
-              cookieStore.set({
-                name,
-                value: '',
-                ...options,
-                maxAge: 0,
-              });
-            } catch (error) {
-              console.error('Error removing cookie:', error);
-            }
-          },
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
         },
+        set(name: string, value: string, options: CookieOptions) {
+          try {
+            cookieStore.set({
+              name,
+              value,
+              ...options
+            });
+          } catch (error) {
+            console.error('Error setting cookie:', error);
+          }
+        },
+        remove(name: string, options: CookieOptions) {
+          try {
+            cookieStore.set({
+              name,
+              value: '',
+              ...options,
+              maxAge: 0
+            });
+          } catch (error) {
+            console.error('Error removing cookie:', error);
+          }
+        }
       }
-    );
-  };
+    }
+  );
+};
