de9c0b3a80f67381eb5c84feafea3a78d408cce4
cschorr4 <cschorr@purdue.edu>
2025-01-04 11:49:36 -0500

    update

--- components/StockSelector.tsx
+++ components/StockSelector.tsx
@@ -9,7 +9,7 @@ import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import { Input } from "@/components/ui/input";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { cn } from "@/lib/utils";
 
 interface StockSelectorProps {
